326139515b382cc259fd8e7faecff582364dd012
ajacobs12 <allisonjacobs97@gmail.com>
2019-12-08 12:11:32 -0500

    added constraints
    constraints 8 through 18 have been added, further tweaking required

--- FYDP_ModV1/src/version1.java
+++ FYDP_ModV1/src/version1.java
@@ -26,6 +26,7 @@ public class version1 {
 		int primaryUb = n3-5;
 		int frenchCohortlb = n3-4;
 		int frenchCohortub = n3-2;
+		int frenchNum = frenchCohortub - frenchCohortlb;
 		//int classUb = n3-2;
 		int prepCohort = n3;
 		int awayCohort = n3-1;
@@ -385,30 +386,226 @@ cplex.addEq(math4[k],60);
 cplex.addEq(math5[k],60);
 }
 
-
 //constraint 8, language for primary cohorts
+IloLinearNumExpr[] lang1 = new IloLinearNumExpr[primaryUb];
+IloLinearNumExpr[] lang2 = new IloLinearNumExpr[primaryUb];
+IloLinearNumExpr[] lang3 = new IloLinearNumExpr[primaryUb];
+IloLinearNumExpr[] lang4 = new IloLinearNumExpr[primaryUb];
+IloLinearNumExpr[] lang5 = new IloLinearNumExpr[primaryUb];
+
+for(int k=0;k<primaryUb;k++) {
+	lang1[k] = cplex.linearNumExpr();
+	lang2[k] = cplex.linearNumExpr();
+	lang3[k] = cplex.linearNumExpr();
+	lang4[k] = cplex.linearNumExpr();
+	lang5[k] = cplex.linearNumExpr();
+	
+	for(int j=0;j<n2;j++) {
+		for(int a1 =1;a1<=6;a1++) {
+			lang1[k].addTerm(lengtht[a1], x[1][j][k][a1]);
+			}
+		for(int b1=7; b1<=12;b1++){
+			lang2[k].addTerm(lengtht[b1], x[1][j][k][b1]);
+			}
+		for(int c=13; c<=18;c++){
+			lang3[k].addTerm(lengtht[c], x[1][j][k][c]);
+			}
+		for(int d = 19; d<=24;d++){
+			lang4[k].addTerm(lengtht[d], x[1][j][k][d]);
+			}
+		for(int e=25; e<=30; e++){
+			lang5[k].addTerm(lengtht[e], x[1][j][k][e]);
+			}
+		}
+	}
+
+for(int k=0;k<teachingCohort;k++) {
+	cplex.addEq(lang1[k],100);
+	cplex.addEq(lang2[k],100);
+	cplex.addEq(lang3[k],100);
+	cplex.addEq(lang4[k],100);
+	cplex.addEq(lang5[k],100);
+	}
 
 //constraint 9, language for french applicable cohorts
+IloLinearNumExpr[] lang6 = new IloLinearNumExpr[frenchNum];
+
+for(int k=0;k<frenchNum;k++){
+	lang6[k] = cplex.linearNumExpr();
+	for(int j=0;j<n2;j++) {
+		for(int t =0;t<n4;t++) {
+			lang6[k].addTerm(lengtht[t], x[1][j][k][t]);
+			}
+		}
+	}
+
+for(int k = 0; k<frenchNum;k++){
+	cplex.addGe(lang6[k], 300);
+	}
 
 //constraint 10, science 
+IloLinearNumExpr[] sci = new IloLinearNumExpr[teachingCohort];
+
+for(int k = 0; k<teachingCohort; k++){
+	sci[k] = cplex.linearNumExpr();
+	for(int j = 0; j<n2;j++){
+		for(int t = 0; t<n4;t++){
+			sci[k].addTerm(lengtht[t], x[2][j][k][t]);
+			}
+		}
+	}
+
+for(int k = 0; k<teachingCohort;k++){
+	cplex.addGe(sci[k], 100);
+	cplex.addLe(sci[k], 150);
+	}
 
 //constraint 11, art
+IloLinearNumExpr[] art = new IloLinearNumExpr[teachingCohort];
+
+for(int k = 0; k<teachingCohort; k++){
+	art[k] = cplex.linearNumExpr();
+	for(int j=0;j<n2;j++){
+		for(int t=0;t<n4;t++){
+			art[k].addTerm(lengtht[t], x[3][j][k][t]);
+			}
+		}
+	}
+
+for(int k = 0;k<teachingCohort; k++){
+	cplex.addGe(art[k], 300);
+	}
 
 //constraint 12, social studies
+IloLinearNumExpr [] soc = new IloLinearNumExpr[teachingCohort];
+
+for(int k=0; k<teachingCohort;k++){
+	soc[k] = cplex.linearNumExpr();
+	for(int j=0; j<n2;j++){
+		for(int t=0;t<n4;t++){
+			soc[k].addTerm(lengtht[t], x[4][j][k][t]);
+			}
+		}
+	}
+
+for(int k = 0;k<teachingCohort;k++){
+	cplex.addGe(soc[k],100);
+	}
 
 //constraint 13, phys-ed
+IloLinearNumExpr [] phys = new IloLinearNumExpr[teachingCohort];
+
+for(int k=0;k<teachingCohort;k++){
+	phys[k] = cplex.linearNumExpr();
+	for(int j=0;j<n2;j++){
+		for(int t=0;t<n4;t++){
+			phys[k].addTerm(lengtht[t], x[5][j][k][t]);
+			}
+		}
+	}
+
+for(int k=0;k<teachingCohort;k++){
+	cplex.addGe(phys[k], 150);
+	cplex.addLe(phys[k],300);
+	}
 
 //constraint 14, french for applicable classes
+IloLinearNumExpr [] french2 = new IloLinearNumExpr[frenchNum];
+
+for(int k =0; k<frenchNum;k++){
+	french2[k] = cplex.linearNumExpr();
+	for(int j=0;j<frenchNum; j++){
+		for(int t=0;t<n4;t++){
+			french2[k].addTerm(lengtht[t], x[6][j][k][t]);
+			}
+		}
+	}
+
+for(int k=0;k<frenchNum;k++){
+	cplex.addGe(french2[k], 200);
+	}
+
 
 //constraint 15, prep
+IloLinearNumExpr [] prepCon = new IloLinearNumExpr[n2];
+
+for(int j = 0;j<n2;j++){
+	prepCon[j] = cplex.linearNumExpr();
+	for(int t=0;t<n4;t++){
+		prepCon[j].addTerm(lengtht[t], x[prepSubject][j][prepCohort][t]);
+		}
+	}
+
+for(int j=0;j<n2;j++){
+	cplex.addGe(prepCon[j], prep[j]);
+	}
 
 //constraint 16, teaching mins 
+IloLinearNumExpr [] teach = new IloLinearNumExpr[n2];
+
+for(int j=0;j<n2;j++){
+	teach[j] = cplex.linearNumExpr();
+	for(int t=0;t<n2;t++){
+		for(int i=0;i<n;i++){
+			for(int k=0;k<teachingCohort;k++){
+				teach[j].addTerm(lengtht[t], x[i][j][k][t]);
+				}
+			}
+		}
+	}
+
+for(int j=0;j<n2;j++){
+	cplex.addLe(teach[j], teachMin[j]);
+	}
 
 //constraint 17, gym capacity
+IloLinearNumExpr [] gymCapCon = new IloLinearNumExpr[n4];
+
+for(int t=0;t<n4;t++){
+	gymCapCon[t] = cplex.linearNumExpr();
+	for(int j=0;j<n2;j++){
+		for(int k=0;k<n3;k++){
+			gymCapCon[t].addTerm(1, x[5][j][k][t]);
+			}
+		}
+	}
+
+for(int t=0;t<n4;t++){
+	cplex.addLe(gymCapCon[t], gymCap);
+	}
 
 //constraint 18, prep time objective
+IloLinearNumExpr [] prep1 = new IloLinearNumExpr[n2];
+IloLinearNumExpr [] prep2 = new IloLinearNumExpr[n2];
+IloLinearNumExpr [] prep3 = new IloLinearNumExpr[n2];
+IloLinearNumExpr [] prep4 = new IloLinearNumExpr[n2];
+IloLinearNumExpr [] prep5 = new IloLinearNumExpr[n2];
+
+for(int j=0;j<n2;j++){
+	prep1[j] = cplex.linearNumExpr();
+	prep2[j] = cplex.linearNumExpr();
+	prep3[j] = cplex.linearNumExpr();
+	prep4[j] = cplex.linearNumExpr();
+	prep5[j] = cplex.linearNumExpr();
+	for(int a1 =1;a1<=6;a1++) {
+		prep1[j].addTerm(1, x[1][j][prepCohort][a1]);
+   }
+	for(int b1=7; b1<=12;b1++){
+		prep2[j].addTerm(lengtht[b1], x[1][j][prepCohort][b1]);
+		}
+	for(int c=13; c<=18;c++){
+		prep3[j].addTerm(lengtht[c], x[1][j][prepCohort][c]);
+		}
+	for(int d = 19; d<=24;d++){
+		prep4[j].addTerm(lengtht[d], x[1][j][prepCohort][d]);
+		}
+	for(int e=25; e<=30; e++){
+		prep5[j].addTerm(lengtht[e], x[1][j][prepCohort][e]);
+		}
+	}
 
 //constraint 19, language for primary has to be back to back
+
 }
 		
 		catch (IloException exc) {
